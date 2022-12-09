e42929813508cc5be1a3133bc058837043f4399f
lemonbadger <115188486+lemonbadger@users.noreply.github.com>
2022-12-09 16:46:11 +0100

    9/12
    Gjorde en ny funktion mha W3

--- DarkMode.js
+++ DarkMode.js
@@ -1,12 +1,4 @@
 function DarkMode(){
-    document.getElementById("darkmode").style.color = "darkblue";
-    document.getElementById("darkmode").style.fontFamily = "Helvetica,sans-serif";
-    document.body.style.backgroundColor = "#6891BE";
-
-    let list = document.getElementsByClassName("darkmode");
-    for(let i = 0; i < list.length; ++i){
-        list[i].style.color = "darkblue";
-        list[i].style.fontFamily = "Helvetica,sans-serif";
-    }
-
+    var element = document.body;
+    element.classList.toggle("darkmode");
 }
