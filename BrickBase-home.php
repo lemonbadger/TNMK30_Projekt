b3db0681e81a2f71455fb93ae147fe47fcc6f2df
lemonbadger <115188486+lemonbadger@users.noreply.github.com>
2022-12-09 16:45:30 +0100

    9/12
    Uppdaterat länkar och gjort om filen till en php för att försöka få DarkMode.js att fungera. Den fungerar inte än. Planen är att titta vidare på detta i helgen

--- BrickBase-home.php
+++ BrickBase-home.php
@@ -5,15 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="NavigationMenu.css">
+    <script src="DarkMode.js"></script>
     <title>BrickBase</title>
 </head>
-<body>
+<body class="darkmode">
     <div class="navbar">
         <div class="image_placeholder"><h3>logo_placeholder</h3></div>
-        <a href="BrickBase-home.html">Home</a>
+        <a href="BrickBase-home.php">Home</a>
         <a href="HowToSearch.php">How to search</a>
         <a href="AboutUs.php">About us</a>
-        <input type="checkbox" id="darkmode_toggle">
+        <input type="checkbox" id="darkmode_toggle" class="darkmode" onclick="DarkMode()">
         <label for="darkmode_toggle"><br>Night Mode</label>
     </div>
    <h1>BrickBase</h1>
