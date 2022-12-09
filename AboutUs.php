10bc6f380944739317871db6b4488854aa3443a6
lemonbadger <115188486+lemonbadger@users.noreply.github.com>
2022-12-09 16:43:42 +0100

    Update AboutUs.php

--- AboutUs.php
+++ AboutUs.php
@@ -1,20 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="NavigationMenu.css">
-    <link rel="stylesheet" href="HowToSearch.css">
-    <script src="DarkMode.js"></script>
-    <title>BrickBase</title>
-</head>
-<body class="darkmode">
-    <div class="navbar">
-        <div class="image_placeholder"><h3>logo_placeholder</h3></div>
-        <a href="BrickBase-home.php">Home</a>
-        <a href="HowToSearch.php">How to search</a>
-        <a href="AboutUs.php">About us</a>
-        <input type="checkbox" id="darkmode_toggle"class = "darkmode" onclick="DarkMode()">
-        <label for="darkmode_toggle"><p id="NightMode">Night Mode</p></label>
-    </div>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="NavigationMenu.css">
+        <link rel="stylesheet" href="AboutUs.css">
+        <script src="DarkMode.js"></script>
+        <title>BrickBase</title>
+    </head>
+    <body>
+        <div class="navbar">
+            <div class="image_placeholder"><h3>logo_placeholder</h3></div>
+            <a href="BrickBase-home.php">Home</a>
+            <a href="HowToSearch.php">How to search</a>
+            <a href="AboutUs.php">About us</a>
+            <input type="checkbox" id="darkmode_toggle"class = "darkmode" onclick="DarkMode()">
+            <label for="darkmode_toggle"><p id="NightMode">Night Mode</p></label>
+        </div>
+        <h1>About Us</h1>
+        <div class="columnleft">
+        </div>
+        <div class="columnright">
+        <h2>Who are we?</h2>
+        <p>We are four students from Linköping University, Sweden. We are all studying to 
+            get a degree of science in engineering and this site is one of our projects. 
+            The purpose for us with this project is to learn how to develop a webpage and 
+            adapt it for as many people as possible.
+            The purpose is also that you, yes you, can use our webpage to search in our 
+            database after a specific lego piece you have. Our webpage will then provide you
+            with information about your piece such as what set it belongs to.
+        </div>
+    </body>
+</html>
