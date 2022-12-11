970d3f97456c4a0eeeda7fab7616fcb5085f0c36
lemonbadger <115188486+lemonbadger@users.noreply.github.com>
2022-12-11 15:04:43 +0100

    10/12 darkmode vid reload
    Vi vill att darkmode ska följa med från sida till sida och om man uppdaterar sidan. Därför måste vi spara personens val av darkmode. Hittade en bit kod i ett forum som kanske hjälper. Kan tyvärr inte se om detta fungerar hemifrån.

--- DarkMode.js
+++ DarkMode.js
@@ -1,4 +1,14 @@
 function DarkMode(){
     var element = document.body;
-    element.classList.toggle("darkmode");
+    element.classList.toggle("darkmode"); //Funktion tagen från W3School
+    
+    if (body.classList.contains('dark')){ //lösnning till att behålla darkmode 
+        body.classList.remove('dark');    //vid updatering av sida (hittat i ett forum)
+        localStorage.setItem("theme","light");
+        button.innerHTML="Turn on dark mode";
+    }else{
+        body.classList.add('dark');
+        localStorage.setItem("theme","dark");
+        button.innerHTML="Turn off dark mode";
+    }
 }
