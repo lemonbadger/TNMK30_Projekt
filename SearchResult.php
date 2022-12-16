0f3b3765da90dcfbe672b663bc80bbc7760cba92
Juliaw803e <41002169+Juliaw803e@users.noreply.github.com>
2022-12-16 17:15:10 +0100

    16/12 innan lovet

--- SearchResult.php
+++ SearchResult.php
@@ -6,14 +6,10 @@
 <meta charset= "utf-8">
 	<title> Hemsida </title> 
 	<link rel="stylesheet" href="NavigationMenu.css">
-</head>
-
-<body>
-
-
-
 <title>BrickBase</title>
 </head>
+
+
 <body>
     <div class="navbar">
         <div class="image_placeholder"><h3>logo_placeholder</h3></div>
@@ -29,7 +25,7 @@
   
     <h1>BrickBase</h1>
 
-    <h2>Din bit ingår i: </h2>
+    
 	
 	
 	
@@ -54,51 +50,10 @@
 	 echo '<img src= '.$SelectedImage.' />';
 	 echo "ID: " . $SelectedPartID;
 	
+	echo '<h2>Din bit ingår i: </h2>';
 
 	$connection = mysqli_connect("mysql.itn.liu.se", "lego", "", "lego"); 
 	
-	/* Gammal query
-		$query = "SELECT
-    inventory.ColorID,
-    colors.Colorname,
-    parts.Partname,
-    inventory.ItemID,
-    inventory.ItemTypeID,
-    images.has_gif,
-    images.has_jpg,
-	sets.SetID,
-	sets.Setname
-FROM
-    inventory,
-    colors,
-    parts,
-    images,
-	sets
-WHERE 
-    parts.Partname = '$SelectedName'
-AND
-    inventory.ColorID = colors.ColorID
-AND
-    inventory.ItemID = parts.PartID
-AND
-    inventory.ItemID = sets.SetID
-AND
-    inventory.ItemTypeID = 'S'
-AND
-    inventory.ItemID = images.ItemID
-AND 
-	inventory.ColorID = images.ColorID
-AND 
-    colors.Colorname = '$SelectedColor'
-	
-AND parts.PartID = '$SelectedPartID'
-
-AND inventory.ItemID = sets.SetID";
-*/		
-	
-
-
-	//Ny query
 	$query	= "SELECT
     inventory.SetID,
     inventory.ColorID,
@@ -121,12 +76,6 @@ AND
 images.ItemID = sets.SetID";
 
 
-
-
-//<img src='$prefix/SL/$setID.jpg'>
-		
-		echo '<h3>Hej</h3>'; 
-	
 	
 		//	Till setten																
 		$result = mysqli_query($connection, $query);	
@@ -147,23 +96,16 @@ images.ItemID = sets.SetID";
 				}
 				 
 					$SetImageUrl = "http://www.itn.liu.se/~stegu76/img.bricklink.com/S/";
-					
 				
 					$SetImageUrl .= $url; 
-					
-					print ("$SetImageUrl");
-					
-					/*echo '<h3>'.$SetID.'</h3>'; 
-					echo '<h3>'.$Setname.'</h3>'; 
-					echo '<img src='.$setimageUrl.' />';*/
-					
 					
 					$Setname=$row['Setname']; 
 					$SetID=$row['SetID'];
 					echo '<div>
 						<tr>
-						<td><img src='.$SetImageUrl.' /></td>
 						<td><h3>'.$Setname.'</h3></td>
+						<td><img src='.$SetImageUrl.' /></td>
+						
 						<td><h3>'.$SetID.'</h3></td>
 						</tr>
 					</div>';
