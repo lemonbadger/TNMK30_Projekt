77de8f4ad70d17aeed2af82d3d28775b5615e97c
DikPhase <117440710+DikPhase@users.noreply.github.com>
2022-12-20 13:27:28 +0100

    Update BrickBase-home.php
    Uppadetrade klasser och id:n så det fungerar med nya css filen

--- BrickBase-home.php
+++ BrickBase-home.php
@@ -4,26 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="NavigationMenu.css">
+    <link rel="stylesheet" href="BrickBase.css">
     <script src="DarkMode.js"></script>
     <title>BrickBase</title>
 </head>
-<body class="darkmode">
+<body>
     <div class="navbar">
         <div class="image_placeholder"><h3>logo_placeholder</h3></div>
-        <a href="BrickBase-home.php">Home</a>
-        <a href="HowToSearch.php">How to search</a>
-        <a href="AboutUs.php">About us</a>
+        <a class="NavButton" href="BrickBase-home.php">Home</a>
+        <a class="NavButton" href="HowToSearch.php">How to search</a>
+        <a class="NavButton" href="AboutUs.php">About us</a>
         <input type="checkbox" id="darkmode_toggle" class="darkmode" onclick="DarkMode()">
-        <label for="darkmode_toggle"><br>Night Mode</label>
+        <label for="darkmode_toggle">Night Mode</label>
     </div>
    <h1>BrickBase</h1>
-    <p>Hej sök efter din bit!</p>
+    <p class="ShortIntro">SEARCH FOR A PIECE!</p>
 
-<form class="searchbar" action="FilterSearch.php">
-	<input name="filter" type="text">
-	<input type="submit">
+<form class="SearchBox" action="FilterSearch.php">
+	<input name="filter" type="text" id="SearchBar">
+	<input type="submit" id="SubmitButton" value="SEARCH">
 </form>
-
 </body>
 </html>
