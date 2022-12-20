b557a953793d46f95511d19d40748591c8caf1c0
DikPhase <117440710+DikPhase@users.noreply.github.com>
2022-12-20 13:20:14 +0100

    Update AboutUs.php
    Uppdaterade så att alla länkar i menyn nu har class till nya css filen

--- AboutUs.php
+++ AboutUs.php
@@ -4,26 +4,25 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="NavigationMenu.css">
-        <link rel="stylesheet" href="AboutUs.css">
+        <link rel="stylesheet" href="BrickBase.css">
         <script src="DarkMode.js"></script>
         <title>BrickBase</title>
     </head>
     <body>
         <div class="navbar">
             <div class="image_placeholder"><h3>logo_placeholder</h3></div>
-            <a href="BrickBase-home.php">Home</a>
-            <a href="HowToSearch.php">How to search</a>
-            <a href="AboutUs.php">About us</a>
+            <a class="NavButton" href="BrickBase-home.php">Home</a>
+            <a class="NavButton" href="HowToSearch.php">How to search</a>
+            <a class="NavButton" href="AboutUs.php">About us</a>
             <input type="checkbox" id="darkmode_toggle"class = "darkmode" onclick="DarkMode()">
-            <label for="darkmode_toggle"><p id="NightMode">Night Mode</p></label>
+            <label for="darkmode_toggle">Night Mode</label>
         </div>
         <h1>About Us</h1>
         <div class="columnleft">
         </div>
         <div class="columnright">
-        <h2>Who are we?</h2>
-        <p>We are four students from Linköping University, Sweden. We are all studying to 
+        <h3>Who are we?</h3>
+        <p class="ShortIntro">We are four students from Linköping University, Sweden. We are all studying to 
             get a degree of science in engineering and this site is one of our projects. 
             The purpose for us with this project is to learn how to develop a webpage and 
             adapt it for as many people as possible.
