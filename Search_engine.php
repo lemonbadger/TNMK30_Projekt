7bd924bcdf78599761c0cfaebbe752e8a0815782
DikPhase <117440710+DikPhase@users.noreply.github.com>
2022-12-20 13:17:25 +0100

    Delete Search_engine.php
    Användes aldrig

--- Search_engine.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$connect = mysqli_connect("mysql.itn.liu.se","lego","","lego");
-
-
-$query = "SELECT parts.Partname, inventory.ItemID FROM inventory, parts WHERE parts.Partname = LIKE %2 X 2% AND inventory.ItemID = parts.PartID";
-    ?>
