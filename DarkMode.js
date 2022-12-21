af10fd9905269c833c510d597ce3aab518d5220a
DikPhase <117440710+DikPhase@users.noreply.github.com>
2022-12-21 15:24:38 +0100

    Update DarkMode.js
    Uppdaterad

--- DarkMode.js
+++ DarkMode.js
@@ -1,4 +1,17 @@
 function DarkMode(){
-    var element = document.body;
-    element.classList.toggle("darkmode");
+    document.body.classList.toggle("darkmode");
+
+    var h1s = document.getElementsByTagName("h1");
+    for (var i = 0; i < h1s.length; i++)
+        h1s[i].classList.toggle("darkmode"); 
+
+    var h3s = document.getElementsByTagName("h3");
+    for (var i = 0; i < h3s.length; i++)
+        h3s[i].classList.toggle("darkmode"); 
+    
+    var ps = document.getElementsByTagName("p");
+    for (var i = 0; i < ps.length; i++)
+        ps[i].classList.toggle("darkmode"); 
+
+        //Funktion tagen från W3School
 }
