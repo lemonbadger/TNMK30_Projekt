5f5bc0a2dcb1a5a1505d401633ec5a90a5c28073
DikPhase <117440710+DikPhase@users.noreply.github.com>
2022-12-21 15:23:06 +0100

    Update BrickBase-home.php
    Uppdaterad

--- BrickBase-home.php
+++ BrickBase-home.php
@@ -8,22 +8,21 @@
     <script src="DarkMode.js"></script>
     <title>BrickBase</title>
 </head>
-<body class="darkmode">
+<body>
     <div class="navbar">
         <div class="image_placeholder"><h3>logo_placeholder</h3></div>
         <a class="NavButton" href="BrickBase-home.php">Home</a>
         <a class="NavButton" href="HowToSearch.php">How to search</a>
         <a class="NavButton" href="AboutUs.php">About us</a>
         <input type="checkbox" id="darkmode_toggle" class="darkmode" onclick="DarkMode()">
-        <label for="darkmode_toggle" id="NightMode">Night Mode</label>
+        <label for="darkmode_toggle">Night Mode</label>
     </div>
    <h1>BrickBase</h1>
-    <p class="ShortIntro">Search for a Lego piece!</p>
+    <p class="ShortIntro">SEARCH FOR A PIECE!</p>
 
 <form class="SearchBox" action="FilterSearch.php">
 	<input name="filter" type="text" id="SearchBar">
 	<input type="submit" id="SubmitButton" value="SEARCH">
 </form>
-
 </body>
 </html>
