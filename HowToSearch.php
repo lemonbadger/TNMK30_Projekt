a97c8ad82ee40f2f517dbf848797623a93efdccc
DikPhase <117440710+DikPhase@users.noreply.github.com>
2022-12-21 15:26:34 +0100

    Update HowToSearch.php
    Uppdaterad

--- HowToSearch.php
+++ HowToSearch.php
@@ -8,13 +8,13 @@
     <script src="DarkMode.js"></script>
     <title>BrickBase</title>
 </head>
-<body class="darkmode">
+<body>
     <div class="navbar">
         <div class="image_placeholder"><h3>logo_placeholder</h3></div>
         <a class="NavButton" href="BrickBase-home.php">Home</a>
         <a class="NavButton" href="HowToSearch.php">How to search</a>
         <a class="NavButton" href="AboutUs.php">About us</a>
         <input type="checkbox" id="darkmode_toggle"class = "darkmode" onclick="DarkMode()">
-        <label for="darkmode_toggle"><p id="NightMode">Night Mode</p></label>
+        <label for="darkmode_toggle">Night Mode</label>
     </div>
     <h1>How To Search</h1>
