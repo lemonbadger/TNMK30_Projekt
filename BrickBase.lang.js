9936e785204ba8a2fc1aacfb654a454bad89c89d
lemonbadger <115188486+lemonbadger@users.noreply.github.com>
2022-12-26 17:10:27 +0100

    26/12
    Ny javascript kod för byte av språk. Detta är endast anpassat för hemsidan än så länge. Inte komplett

---
+++ BrickBase.lang.js
@@ -0,0 +1,17 @@
+var data = {
+                "english":
+                {
+                    "title": "BrickBase" ,
+                    "ShortIntro": "Search for a piece!"
+                },
+                "swedish":
+                {
+                    "title": "BrickBase" ,
+                    "ShortIntro": "Sök efter en bit!"
+                },
+                "german":
+                {
+                    "title": "BrickBase" ,
+                    "ShortIntro": "Suche ein stück!"
+                }
+}
