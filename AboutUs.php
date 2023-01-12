17203062373650797ec1380405aa82292f7d2927
DikPhase <117440710+DikPhase@users.noreply.github.com>
2023-01-12 15:58:37 +0100

    Färdig sida!

--- AboutUs.php
+++ AboutUs.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="BrickBase.css">
-        <script src="Scripts.js"></script>
-        <title>BrickBase</title>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="BrickBase.css">
+    <script src="Scripts.js"></script>
+    <title>BrickBase</title>
     </head>
     <body>
         <div class="navbar">
@@ -24,17 +24,13 @@
             <label for="darkmode_toggle">Night Mode</label>
         </div>
         <h1>About Us</h1>
-        <div class="columnleft">
-        </div>
-        <div class="columnright">
-        <h3>Who are we?</h3>
-        <p class="ShortIntro">We are four students from Linköping University, Sweden. We are all studying to 
+        <p class="AboutUsTxt">We are four students from Linköping University, Sweden. We are all studying to 
             get a degree of science in engineering and this site is one of our projects. 
             The purpose for us with this project is to learn how to develop a webpage and 
             adapt it for as many people as possible.
-            The purpose is also that you, yes you, can use our webpage to search in our 
+            The purpose is also that you, yes you, can use our webpage to search in a 
             database after a specific lego piece you have. Our webpage will then provide you
-            with information about your piece such as what set it belongs to.
-        </div>
+            with the information about what set or sets your piece is included in.
+        </p>
     </body>
 </html>
