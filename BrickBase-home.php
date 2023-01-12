8aab761d94c1b35a4de2f9a51ce3ec1449e4ded6
DikPhase <117440710+DikPhase@users.noreply.github.com>
2023-01-12 11:21:14 +0100

    Update BrickBase-home.php
    Allt innan merge

--- BrickBase-home.php
+++ BrickBase-home.php
@@ -5,12 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="BrickBase.css">
-    <script src="DarkMode.js"></script>
+    <script src="Scripts.js"></script>
     <title>BrickBase</title>
 </head>
 <body>
     <div class="navbar">
-        <div class="image_placeholder"><h3>logo_placeholder</h3></div>
+    <div id="google_translate_element"></div>
+        <script type="text/javascript">
+            function googleTranslateElementInit() {
+            new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+            }
+        </script>
+        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
         <a class="NavButton" href="BrickBase-home.php">Home</a>
         <a class="NavButton" href="HowToSearch.php">How to search</a>
         <a class="NavButton" href="AboutUs.php">About us</a>
