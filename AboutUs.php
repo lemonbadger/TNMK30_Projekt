61e6d3b9cc6c91a4ec5f4ab7971c411598e1f41b
DikPhase <117440710+DikPhase@users.noreply.github.com>
2023-01-12 11:45:38 +0100

    Uppdaterad till merge

--- AboutUs.php
+++ AboutUs.php
@@ -5,12 +5,18 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="BrickBase.css">
-        <script src="DarkMode.js"></script>
+        <script src="Scripts.js"></script>
         <title>BrickBase</title>
     </head>
     <body>
         <div class="navbar">
-            <div class="image_placeholder"><h3>logo_placeholder</h3></div>
+        <div id="google_translate_element"></div>
+        <script type="text/javascript">
+            function googleTranslateElementInit() {
+            new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+            }
+        </script>
+        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
             <a class="NavButton" href="BrickBase-home.php">Home</a>
             <a class="NavButton" href="HowToSearch.php">How to search</a>
             <a class="NavButton" href="AboutUs.php">About us</a>
@@ -18,6 +24,9 @@
             <label for="darkmode_toggle">Night Mode</label>
         </div>
         <h1>About Us</h1>
+        <div class="columnleft">
+        </div>
+        <div class="columnright">
         <h3>Who are we?</h3>
         <p class="ShortIntro">We are four students from Linköping University, Sweden. We are all studying to 
             get a degree of science in engineering and this site is one of our projects. 
@@ -26,5 +35,6 @@
             The purpose is also that you, yes you, can use our webpage to search in our 
             database after a specific lego piece you have. Our webpage will then provide you
             with information about your piece such as what set it belongs to.
+        </div>
     </body>
 </html>
